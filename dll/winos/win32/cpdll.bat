bc62bd0a773e81699d4e1e464ce8a961577befbb
Johan van der Hoeven <lonoami@gmail.com>
2010-09-30 11:58:17 -1000

    Added .lib files to winXX dll dirs. (Are also generated by build -- must match .dll) Added MSVC 10 project files for DeviceCheck program

--- dll/winos/win32/cpdll.bat
+++ dll/winos/win32/cpdll.bat
@@ -1,4 +1,5 @@
 #This requires ming or such --fix?
 #Copy the currently built .dll 
 #Note: It is up to you to build the 32 bit before copying.
-cp  ../../../src/GoIO_DLL/Win32/Debug/GoIO_DLL.dll .
+cp  ../../../src/GoIO_DLL/Win32/Release/GoIO_DLL.dll .
+cp  ../../../src/GoIO_DLL/Win32/Release/GoIO_DLL.lib .
