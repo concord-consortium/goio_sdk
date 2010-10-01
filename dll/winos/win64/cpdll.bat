4c34900ab4ce6e5cbf4886e9ec770d15d0e51b2b
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-10-01 07:20:39 -0400

    Johan's updates sent via tar.gz archive
    http://dl.dropbox.com/u/382278/dll.winos.win64.tar.gz

--- dll/winos/win64/cpdll.bat
+++ dll/winos/win64/cpdll.bat
@@ -3,3 +3,6 @@
 #Note: It is up to you to build the 32 bit before copying.
 cp  ../../../src/GoIO_DLL/Win32/Release/GoIO_DLL.dll .
 cp  ../../../src/GoIO_DLL/Win32/Release/GoIO_DLL.lib .
+cp  ../../../src/GoIO_DLL/Win32/*.h .
+cp  ../../../src/GoIO_DLL/GoIO_DLL_interface.h .
+cp  ../../../src/GoIO_cpp/*.h .
