e995b0e330c1f6565f02206b63b4ad8d8eb7d42b
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-10-06 01:08:11 -0400

    updating and renaming Windows dlls
    Generated from Visual Studio 2010 solution and projects

--- dll/winos/win64/cpdll.bat
+++
@@ -1,8 +0,0 @@
-#This requires ming or such --fix?
-#Copy the currently built .dll 
-#Note: It is up to you to build the 32 bit before copying.
-cp  ../../../src/GoIO_DLL/Win32/Release/GoIO_DLL.dll .
-cp  ../../../src/GoIO_DLL/Win32/Release/GoIO_DLL.lib .
-cp  ../../../src/GoIO_DLL/Win32/*.h .
-cp  ../../../src/GoIO_DLL/GoIO_DLL_interface.h .
-cp  ../../../src/GoIO_cpp/*.h .
